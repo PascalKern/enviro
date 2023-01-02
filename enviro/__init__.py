ccdf0b11ef0e7fed18b17a14d7dd035182454352
Pascal Kern <pkscal.kern@gmail.com>
2023-01-02 16:20:43 +0100

    Improves feature toggles to not fail with old configs

--- enviro/__init__.py
+++ enviro/__init__.py
@@ -306,9 +306,9 @@ def get_sensor_readings():
 
   readings = get_board().get_sensor_readings(seconds_since_last)
 
-  if config.enable_battery_voltage:
+  if hasattr(config, 'enable_battery_voltage') and config.enable_battery_voltage:
     readings["voltage"] = get_battery_voltage()
-  if config.enable_cpu_temp:
+  if hasattr(config, 'enable_cpu_temp') and config.enable_cpu_temp:
     readings["cpu_temp"] = get_cpu_temperature()
 
 
