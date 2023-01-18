554bee382457de6cd7e07e361f427bc43dbd5fa3
Pascal Kern <pkscal.kern@gmail.com>
2023-01-18 18:13:04 +0100

    Adds the feature (with toggle) to measure the CPU temperature also

--- enviro/__init__.py
+++ enviro/__init__.py
@@ -355,10 +355,11 @@ def get_sensor_readings():
 
 
   readings = get_board().get_sensor_readings(seconds_since_last)
-
+  readings["voltage"] = 0.0 # battery_voltage #Temporarily removed until issue is fixed
+  if hasattr(config, 'enable_cpu_temp') and config.enable_cpu_temp:
+    readings["cpu_temp"] = get_cpu_temperature()
   if hasattr(config, 'enable_battery_voltage') and config.enable_battery_voltage:
     readings["voltage"] = get_battery_voltage()
-
 
   # write out the last time log
   with open("last_time.txt", "w") as timefile:
