524938d11ef8661bc3e2b6625d24b7cf756f7dec
Pascal Kern <pkscal.kern@gmail.com>
2023-01-20 09:57:00 +0100

    Merge my_develop/improve_install_script into: my_local/current_deploying
    Squashed commit of the following:
    
    commit cfc1d0865d7ff9df3a4aa9650cf7e140468a5855
    Author: Pascal Kern <pkscal.kern@gmail.com>
    Date:   Tue Jan 17 17:52:10 2023 +0100
    
        Fixes various issues in the script(s)
    
    commit 639a0030fe274a0b21787b1c2215d121404202df
    Author: Pascal Kern <pkscal.kern@gmail.com>
    Date:   Sun Jan 15 10:52:38 2023 +0100
    
        Adds check (safety but not very nice - yet) to prevent errors when GIT_REV constant doesn't exist
    
        Anyway. It's to be discussed/re-thought if this is really a constant or more a config value?! At the moment I would tend more to it being a config entry but not sure as it feels wrong too.
    
        But as the enviro version is also set there it should be ok!
    
    commit b31ba31746aec39f684ba0817d65a35955f99ace
    Author: Pascal Kern <pkscal.kern@gmail.com>
    Date:   Sun Jan 15 09:37:33 2023 +0100
    
        Adds and updates the installer script functionality
    
        - Do download logs and config
        - Do clean up the device prior to upload to be sure not to have leftover code parts or unwanted stuf laying around
        - Do upload a config per device via UID selected
        - Do add the current Git rev as constant to be logged and or sent by MQTT
    
    commit 7fe388bb57659487abd38cb6d54e1ae5b2a3c48a
    Author: Pascal Kern <pkscal.kern@gmail.com>
    Date:   Tue Jan 3 12:26:25 2023 +0100
    
        Updates script to do the config copy if available
    
    commit 8fd32a5d215d5af9b6146de0d1e1a63a749d3e5e
    Author: Pascal Kern <pkscal.kern@gmail.com>
    Date:   Tue Jan 3 12:16:32 2023 +0100
    
        Fixes one TODO and prepares directory for <uid>-config.py files per board (not version controlled!)
    
    commit 4eb41770a3e31f5dd5e5c0f36d5ce31f69098925
    Author: Pascal Kern <pkscal.kern@gmail.com>
    Date:   Tue Jan 3 12:06:43 2023 +0100
    
        Adds TODO's
    
    commit 3413781e4d7998e817f102114516e5ce5ca6b9eb
    Author: Pascal Kern <pkscal.kern@gmail.com>
    Date:   Tue Jan 3 11:43:47 2023 +0100
    
        Updates script to create a constant (GIT_REV) when uploading include config.py (to be improved)

---
+++ install_on_device_fs_helper.py
@@ -0,0 +1,43 @@
+import os
+
+
+def dir_exists(dir_name):
+  try:
+    return os.stat(dir_name)[0] == 16384
+  except OSError:
+    return False
+
+
+def file_exists(filename):
+  try:
+    return (os.stat(filename)[0] & 0x4000) == 0
+  except OSError:
+    return False
+
+
+def rmdir(dir_name):
+  if dir_exists(dir_name):
+    for i in os.ilistdir(dir_name):
+      if i[1] == 16384:
+        # print(f'Is directory: {i} -> {"{}/{}".format(dir_name, i[0])}')
+        rmdir('{}/{}'.format(dir_name, i[0]))
+      elif i[1] == 32768:
+        # print(f'Is file: {i} -> {"{}/{}".format(dir_name, i[0])}')
+        os.remove('{}/{}'.format(dir_name, i[0]))
+      # else:
+        # print(f'Unknown filehandle number: {i[1]}')
+    os.rmdir(dir_name)
+    return True
+  return False
+
+
+def main(dirs):
+  for dr in dirs:
+    print(f'> Cleaning directory: {dr} (inclusive content)', end='')
+    if rmdir(dr):
+      print(' .. done!')
+    else:
+      print(' .. skipped as directory does not exist on board!')
+
+
+main(['phew', 'umqtt', 'enviro'])
