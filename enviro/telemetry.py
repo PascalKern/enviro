ca8ae8aa7412e1e5684736ac676d14acf6cfb54c
Pascal Kern <pkscal.kern@gmail.com>
2025-07-05 13:45:18 +0200

    Make `get_battery_voltage` sample count configurable and inline ADC conversion constant

--- enviro/telemetry.py
+++ enviro/telemetry.py
@@ -19,23 +19,20 @@ def _config_key_exists_with_enabling_value(key: str) -> bool:
   return hasattr(config, key) and getattr(config, key, False)
 
 
-ADC_VOLT_CONVERSION = 3.3 / 65535
-ADC_VOLTAGE_SAMPLE_COUNT = 10
-
-
-def get_battery_voltage():
+def get_battery_voltage(adc_voltage_sample_count:int = 10):
   old_pad = get_pad(29)
   set_pad(29, 128)  # no pulls, no output, no input
 
   battery_voltage = 0
-  for i in range(0, ADC_VOLTAGE_SAMPLE_COUNT):
+  for i in range(0, adc_voltage_sample_count):
     battery_voltage += _read_vsys_voltage()
-  battery_voltage /= ADC_VOLTAGE_SAMPLE_COUNT
+  battery_voltage /= adc_voltage_sample_count
   battery_voltage = round(battery_voltage, 3)
   set_pad(29, old_pad)
   return battery_voltage
 
 
 def _read_vsys_voltage():
+  adc_volt_conversion = 3.3 / 65535
   adc_vsys = machine.ADC(3)
-  return adc_vsys.read_u16() * 3.0 * ADC_VOLT_CONVERSION
+  return adc_vsys.read_u16() * 3.0 * adc_volt_conversion
