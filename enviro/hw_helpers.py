b52b33149c772a22eb9cb89df3940ada60d9b2b0
Pascal Kern <pkscal.kern@gmail.com>
2025-07-05 15:00:49 +0200

    Add CPU temperature sensing support in telemetry module
    Includes implementation of CPU temperature reading helper and integration into telemetry reading logic.

--- enviro/hw_helpers.py
+++ enviro/hw_helpers.py
@@ -1,5 +1,7 @@
 import machine
 
+# cpu temperature declaration
+CPU_TEMP = machine.ADC(machine.ADC.CORE_TEMP)
 
 def get_pad(gpio):
   return machine.mem32[0x4001c000 | (4 + (4 * gpio))]
--- enviro/telemetry.py
+++ enviro/telemetry.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 import machine
 
 import config
-from enviro.hw_helpers import get_pad, set_pad
+from enviro.hw_helpers import get_pad, set_pad, CPU_TEMP
 
 
 def add_telemetry_readings(readings: OrderedDict) -> OrderedDict:
@@ -12,11 +12,17 @@ def add_telemetry_readings(readings: OrderedDict) -> OrderedDict:
   if _config_key_exists_with_enabling_value('enable_voltage_sensing'):
     telemetry_readings["voltage"] = get_battery_voltage()
 
+  if _config_key_exists_with_enabling_value('enable_cpu_temperature_sensing'):
+    telemetry_readings["cpu_temp"] = get_cpu_temperature()
+
   return {**readings, 'telemetry': {**telemetry_readings}} if telemetry_readings else readings
 
 
 def _config_key_exists_with_enabling_value(key: str) -> bool:
   return hasattr(config, key) and getattr(config, key, False)
+
+
+ADC_VOLT_CONVERSION = 3.3 / 65535
 
 
 def get_battery_voltage(adc_voltage_sample_count:int = 10):
@@ -36,3 +42,7 @@ def _read_vsys_voltage():
   adc_volt_conversion = 3.3 / 65535
   adc_vsys = machine.ADC(3)
   return adc_vsys.read_u16() * 3.0 * adc_volt_conversion
+
+def get_cpu_temperature():
+  reading = CPU_TEMP.read_u16() * ADC_VOLT_CONVERSION
+  return 27 - (reading - 0.706) / 0.001721
