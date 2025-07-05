a12a63a05ec84438a186a4ef4afe1a6dfeee787b
Pascal Kern <pkscal.kern@gmail.com>
2025-07-05 07:55:25 +0200

    Fix inconsistent indentation in `hw_helpers.py` and `telemetry.py` modules

--- enviro/hw_helpers.py
+++ enviro/hw_helpers.py
@@ -7,5 +7,3 @@ def get_pad(gpio):
 
 def set_pad(gpio, value):
   machine.mem32[0x4001c000 | (4 + (4 * gpio))] = value
-
-
--- enviro/telemetry.py
+++ enviro/telemetry.py
@@ -7,16 +7,16 @@ from enviro.hw_helpers import get_pad, set_pad
 
 
 def add_telemetry_readings(readings: OrderedDict) -> OrderedDict:
-    telemetry_readings = OrderedDict()
+  telemetry_readings = OrderedDict()
 
-    if _config_key_exists_with_enabling_value('enable_voltage_sensing'):
-        telemetry_readings["voltage"] = get_battery_voltage()
+  if _config_key_exists_with_enabling_value('enable_voltage_sensing'):
+    telemetry_readings["voltage"] = get_battery_voltage()
 
-    return {**readings, 'telemetry': {**telemetry_readings}} if telemetry_readings else readings
+  return {**readings, 'telemetry': {**telemetry_readings}} if telemetry_readings else readings
 
 
 def _config_key_exists_with_enabling_value(key: str) -> bool:
-    return hasattr(config, key) and getattr(config, key, False)
+  return hasattr(config, key) and getattr(config, key, False)
 
 
 ADC_VOLT_CONVERSION = 3.3 / 65535
@@ -24,18 +24,18 @@ ADC_VOLTAGE_SAMPLE_COUNT = 10
 
 
 def get_battery_voltage():
-    old_pad = get_pad(29)
-    set_pad(29, 128)  # no pulls, no output, no input
+  old_pad = get_pad(29)
+  set_pad(29, 128)  # no pulls, no output, no input
 
-    battery_voltage = 0
-    for i in range(0, ADC_VOLTAGE_SAMPLE_COUNT):
-        battery_voltage += _read_vsys_voltage()
-    battery_voltage /= ADC_VOLTAGE_SAMPLE_COUNT
-    battery_voltage = round(battery_voltage, 3)
-    set_pad(29, old_pad)
-    return battery_voltage
+  battery_voltage = 0
+  for i in range(0, ADC_VOLTAGE_SAMPLE_COUNT):
+    battery_voltage += _read_vsys_voltage()
+  battery_voltage /= ADC_VOLTAGE_SAMPLE_COUNT
+  battery_voltage = round(battery_voltage, 3)
+  set_pad(29, old_pad)
+  return battery_voltage
 
 
 def _read_vsys_voltage():
-    adc_vsys = machine.ADC(3)
-    return adc_vsys.read_u16() * 3.0 * ADC_VOLT_CONVERSION
+  adc_vsys = machine.ADC(3)
+  return adc_vsys.read_u16() * 3.0 * ADC_VOLT_CONVERSION
