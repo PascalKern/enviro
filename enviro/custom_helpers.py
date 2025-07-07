60d88e90c7ad6db4e4bbb3c753375be06c92567a
Pascal Kern <pkscal.kern@gmail.com>
2025-07-07 16:51:27 +0200

    Add `initialize_rtc` helper to set up and sync RTC with fallback logic
    Includes initialization of the `PCF85063A` real-time clock chip, ensures RTC is running, synchronizes with the RP2040's RTC, and retries on invalid datetime reads with a maximum attempt limit. Adjusts imports to include required modules.

--- enviro/custom_helpers.py
+++ enviro/custom_helpers.py
@@ -1,6 +1,26 @@
+import time
+
+from umachine import RTC
+from pcf85063a import PCF85063A
+
 import custom_config
+from enviro import i2c
 
 
 def is_custom_config_active(key: str) -> bool:
   return hasattr(custom_config, key) and getattr(custom_config, key, False)
 
+def initialize_rtc(max_tries: int = 10):
+  global rtc
+  # intialise the pcf85063a real time clock chip
+  rtc = PCF85063A(i2c)
+  i2c.writeto_mem(0x51, 0x00, b'\x00')  # ensure rtc is running (this should be default?)
+  rtc.enable_timer_interrupt(False)
+  t = rtc.datetime()
+  # TODO Maybe need to check other indices like t[6], t[7] (t[1] can be 0 no clue why but can)?
+  while t[2] == 0 and max_tries > 0:
+    max_tries -= 1
+    time.sleep_ms(2)
+    t = rtc.datetime()
+  # BUG ERRNO 22, EINVAL, when date read from RTC is invalid for the pico's RTC.
+  RTC().datetime((t[0], t[1], t[2], t[6], t[3], t[4], t[5], 0))  # synch PR2040 rtc too
