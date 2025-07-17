0d2cc59d7b7fa791e442515c7ddb1361e8ca88d2
Pascal Kern <pkscal.kern@gmail.com>
2025-07-17 21:33:41 +0200

    Fix `ujson.dump` call in `uploads` to properly use the `separators` argument instead of a tuple.

--- enviro/__init__.py
+++ enviro/__init__.py
@@ -424,7 +424,10 @@ def cache_upload(readings):
   uploads_filename = f"uploads/{helpers.datetime_file_string()}.json"
   helpers.mkdir_safe("uploads")
   with open(uploads_filename, "w") as upload_file:
-    ujson.dump(payload, upload_file, separators=(',', ':'))
+    # json.dump(payload, upload_file) # TODO what it was changed to
+    upload_file.write(ujson.dumps(payload, (',', ':')))
+    # TODO Why not?
+    # ujson.dump(payload, upload_file, (',', ':'))
 
 # return the number of cached results waiting to be uploaded
 def cached_upload_count():
